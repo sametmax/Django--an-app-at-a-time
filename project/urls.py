c8566b7c477fd2b02e8cd8e94b355bb9cb0dbc1b
floecureuil <flo_lecureuil@hotmail.fr>
2013-08-19 11:56:23 -0700

    Corrections in 'project' folder

--- project/urls.py
+++ project/urls.py
@@ -4,19 +4,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-# Obligatoire pour que l'admin fonctionne
+# Obligatoire pour que l'admin fonctionne.
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
 
-    # affiche l'admin à l'URL /admin/
+    # Affiche l'admin à l'URL /admin/
     url(r'^admin/', include(admin.site.urls)),
 
-    # inclut nos apps, une par une
+    # Inclut nos apps, une par une.
 
-    # Ceci dit que tout URL commençant par 'app1' doir être dirigée sur notre
-    # première app
+    # Ceci dit que tout URL commençant par 'app1' doit être dirigée sur notre
+    # première app.
     url(r'^app1/', include('app1_hello.urls')),
 
     url(r'^app2/', include('app2_hello_again.urls')),
@@ -25,6 +25,6 @@ urlpatterns = patterns('',
     url(r'^app5/', include('app5_get_post_and_cookies.urls')),
     url(r'^app6/', include('app6_template_tools.urls')),
 
-    # Ignorez cela, c'est la page listant toutes les autres apps
+    # Ignorez cela, c'est la page listant toutes les autres apps.
     url(r'^$', include('ignore_me.urls')),
 )
